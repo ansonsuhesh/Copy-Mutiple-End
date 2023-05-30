745464335af7f9e93b47630ac3b8c6a64e46a854
anson_suhesh <74465476+ansonsuhesh@users.noreply.github.com>
2023-05-30 12:33:38 +0530

    Add files via upload

---
+++ Copy Multiple End/Copy Multiple Tek1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Copy_Multiple_End
+{
+    partial class Form1Tek1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Model";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Copy Selected Objects";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Start Handle to Start Handle",
+            "End Handle  to End Handle"});
+            this.comboBox1.Location = new System.Drawing.Point(62, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(189, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1Tek1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 228);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1Tek1";
+            this.Text = "Copy Multiple Tek1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Copy Multiple End/Copy Multiple Tek1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+using TSMUI = Tekla.Structures.Model.UI;
+
+
+namespace Copy_Multiple_End
+{
+    public partial class Form1Tek1 : Form
+    {
+        public Form1Tek1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        //Initializing a model object that establishes a connection between Tekla and our code
+        Model myModel = new Model();
+
+        //Code to verify the connectivity of our model
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(myModel.GetConnectionStatus())
+            {
+                MessageBox.Show("Model Connected");
+            }
+            else
+            {
+                MessageBox.Show("Model not Connected");
+            }
+        }
+
+        //This method duplicates the model objects from one part to another part
+        public void CopyMultiple()
+        {
+            //The initialization of the Picker class and ModelObjectSelector class
+            TSMUI.Picker picker = new TSMUI.Picker();
+            TSMUI.ModelObjectSelector modelObjectSelector = new TSMUI.ModelObjectSelector();
+
+            //Initializing an enumerator for objects to be copied and destination objects
+            ModelObjectEnumerator objectsToBeCopied = modelObjectSelector.GetSelectedObjects();
+            ModelObject sourceObject = null;
+            ModelObjectEnumerator destinationObjects = null;
+            int position = comboBox1.SelectedIndex;
+
+            //Checking if any object is selected to be copied
+            if (objectsToBeCopied.GetSize() == 0)
+            {
+                Operation.DisplayPrompt("You have to select objects before copying!");
+                goto End;
+            }
+
+            //Initiating a prompt to pick a source object and multiple destination objects
+            try
+            {
+                sourceObject = picker.PickObject(TSMUI.Picker.PickObjectEnum.PICK_ONE_PART, "Pick source part");
+                destinationObjects = picker.PickObjects(TSMUI.Picker.PickObjectsEnum.PICK_N_PARTS, "Pick destination objects, can be multiple objects as well");
+            }
+            catch (Exception)
+            {
+                Operation.DisplayPrompt("User interrupt");
+            }
+
+            //The main loop that copies each object to every destination object
+            try
+            {
+                if (destinationObjects != null)
+                {
+                    //Getting the coordinate system of the source object
+                    CoordinateSystem sourceCoordinationSystem = sourceObject.GetCoordinateSystem();
+
+                    while (destinationObjects.MoveNext())
+                    {
+                        CoordinateSystem destionationCoordinateSystem = null;
+                        destinationObjects.Current.Select();
+                        sourceObject.Select();
+                        //If the selected index is 0, then it will execute using the part's coordinate system
+                        //If the selected index is 1, then the code will execute using the part's end coordinate system
+                        if (position == 1 && sourceObject is Beam && destinationObjects.Current is Beam)
+                        {
+                            destionationCoordinateSystem = EndCoordinateSystem(destinationObjects.Current as Part);
+                            sourceCoordinationSystem = EndCoordinateSystem(sourceObject as Part);
+                        }
+                        else
+                        {
+                            destionationCoordinateSystem = destinationObjects.Current.GetCoordinateSystem();
+                            sourceCoordinationSystem = sourceObject.GetCoordinateSystem();
+                        }
+
+                        //The main loop that copies objects using the CopyObject method
+                        while (objectsToBeCopied.MoveNext())
+                        {
+                            if (destinationObjects.Current.Identifier.GUID != sourceObject.Identifier.GUID)
+                            {
+                                var copiedObject = Operation.CopyObject(objectsToBeCopied.Current, sourceCoordinationSystem, destionationCoordinateSystem);
+                            }
+
+                        }
+                        objectsToBeCopied.Reset();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Operation.DisplayPrompt("Unknown error 1");
+                goto End;
+            }
+            myModel.CommitChanges();
+            Operation.DisplayPrompt("Operation Completed");
+        End:;
+        }
+
+        //The new method to determine the coordinate system at the end handle
+        public CoordinateSystem EndCoordinateSystem(Part part)
+        {
+            Beam beam = part as Beam;
+            CoordinateSystem source = part.GetCoordinateSystem();
+
+            //This method is very useful compared to workplanes
+            //While working with workplanes, it is not working properly as the program executes too fast
+            Matrix ToLocal = MatrixFactory.ToCoordinateSystem(source);
+            Point CurrentPoint = ToLocal.Transform(beam.EndPoint);
+
+            //The beam endpoint aligns with the Y and Z axes of the part's coordinate system
+            CurrentPoint.Y = 0;
+            CurrentPoint.Z = 0;
+            Matrix ToCurrentWP = MatrixFactory.FromCoordinateSystem(source);
+            Point Origin = ToCurrentWP.Transform(CurrentPoint);
+            CoordinateSystem endCoordinateSystem = new CoordinateSystem(Origin, source.AxisX, source.AxisY);
+            return endCoordinateSystem;
+        }
+
+        //Initiate the Copy Multiple method when the operator clicks
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CopyMultiple();
+        }
+
+    }
+}
